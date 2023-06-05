c6ac2baabf901366900da4665ce8ed4b59a099ee
Karl Lew <karl@firepick.org>
2023-06-05 08:06:08 -0700

    v1.0.0 += build-html

---
+++ ebt-config.mjs
@@ -0,0 +1,13 @@
+const EbtConfig = {
+  "basePath": "/ebt-site3/",
+  "homePath": "#/wiki/welcome",
+  "content": {
+    "index": "toc",
+  },
+  "github": {
+    "account": "ebt-site",
+    "repository": "ebt-site3"
+  }
+}
+
+export default EbtConfig;
